3628d1b1b2433e94043660a2eb179b5aab4e613d
VyomKorat456 <vyomkorat2020@gmail.com>
2026-02-27 16:57:38 +0530

    fix: enforce H2 isolation for tests using dedicated test config

--- auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
+++ auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
@@ -5,15 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-@SpringBootTest(properties = {
-		"eureka.client.enabled=false",
-		"spring.cloud.discovery.enabled=false",
-		"spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/realms/rarebids",
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.jpa.hibernate.ddl-auto=create-drop",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
 class AuctionServiceApplicationTests {
 
 	@MockitoBean
--- auction-service/src/test/java/com/bid/auction_service/SystemCheckTest.java
+++ auction-service/src/test/java/com/bid/auction_service/SystemCheckTest.java
@@ -13,15 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-@SpringBootTest(properties = {
-        "eureka.client.enabled=false",
-        "spring.cloud.discovery.enabled=false",
-        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/realms/rarebids",
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
 @AutoConfigureMockMvc
 class SystemCheckTest {
 
