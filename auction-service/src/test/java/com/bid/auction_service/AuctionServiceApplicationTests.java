24cb1405d141f3cef73c48a9edfd556ad1b7da4f
VyomKorat456 <vyomkorat2020@gmail.com>
2026-02-27 14:47:04 +0530

    fix: mock JwtDecoder in ApplicationTests to allow CI context-load

--- auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
+++ auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package com.bid.auction_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class AuctionServiceApplicationTests {
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
