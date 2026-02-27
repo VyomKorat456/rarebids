1f3adfb6d44c38962674273c77931064029931b6
VyomKorat456 <vyomkorat2020@gmail.com>
2026-02-27 14:53:06 +0530

    fix: add missing SpringBootTest import in NotificationService

--- notification-service/src/test/java/com/bid/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/bid/notification_service/NotificationServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.bid.notification_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
