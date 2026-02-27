9d6868f26b925ebf117a67b7ef802303a62188a8
VyomKorat456 <vyomkorat2020@gmail.com>
2026-02-27 16:26:41 +0530

    fix: standardize H2 dialect in auction-service tests

--- auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
+++ auction-service/src/test/java/com/bid/auction_service/AuctionServiceApplicationTests.java
@@ -8,7 +8,11 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 @SpringBootTest(properties = {
 		"eureka.client.enabled=false",
 		"spring.cloud.discovery.enabled=false",
-		"spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/realms/rarebids"
+		"spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/realms/rarebids",
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
 })
 class AuctionServiceApplicationTests {
 
